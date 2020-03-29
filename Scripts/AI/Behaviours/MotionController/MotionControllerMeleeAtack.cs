5189862ea93a907dcc18b5dc3dc4d84fc4ffdc0d
Ross Gardler <ross.gardler@microsoft.com>
2020-03-29 14:13:49 -0800

    Melee Attack message now implemented - works with Sword and Shield from Ootii pack, but need to make it work with the Hatchet for the demo. Replace the Wizard with the Third person model in Neo FPS

--- Scripts/AI/Behaviours/MotionController/MotionControllerMeleeAtack.cs
+++ Scripts/AI/Behaviours/MotionController/MotionControllerMeleeAtack.cs
@@ -1,4 +1,8 @@
-﻿using NeoFPS.AI.Behaviour;
+﻿using com.ootii.Actors.AnimationControllers;
+using com.ootii.Actors.Combat;
+using com.ootii.Messages;
+using NeoFPS.AI.Behaviour;
+using System.Collections;
 using UnityEngine;
 
 namespace NeoFPS.AI.OotiiMotionController
@@ -13,7 +17,28 @@ namespace NeoFPS.AI.OotiiMotionController
         [SerializeField, Tooltip("The minimum time between attacks.")]
         float m_Cooldown = 1f;
 
+        private MotionController m_MotionController;
         float m_NextAttackTime = 0;
+
+        internal override bool Init(GameObject owner)
+        {
+            bool isSuccess = false;
+            m_MotionController = owner.GetComponent<MotionController>();
+            isSuccess = m_MotionController != null;
+            Debug.Assert(isSuccess, owner + " has a MotionControllerMeleeAttack AI behaviour but no MotionController.");
+            isSuccess &= base.Init(owner);
+
+            if (isSuccess)
+            {
+                IsActive = true;
+                return true;
+            }
+            else
+            {
+                IsActive = false;
+                return true;
+            }
+        }
 
         internal override string Tick()
         {
@@ -22,10 +47,27 @@ namespace NeoFPS.AI.OotiiMotionController
                 return "Melee attack is on cooldown.";
             }
 
-            Debug.Log("Melee Attack");
+            HandleMeleeAttackMotion();
             m_NextAttackTime = Time.time + m_Cooldown;
 
             return "";
+        }
+
+        private void HandleMeleeAttackMotion()
+        {
+            // FIXME: Can't be finding objects with tag here, need to use a shared variable
+            GameObject target = GameObject.FindGameObjectWithTag("Player");
+
+            CombatMessage message = CombatMessage.Allocate();
+            if (message != null)
+            {
+                message.ID = CombatMessage.MSG_COMBATANT_ATTACK;
+                message.Attacker = m_Owner;
+                message.Defender = target;
+                m_MotionController.SendMessage(message);
+                CombatMessage.Release(message);
+            }
+
         }
     }
 }
