b2e49512dca0244108db5c07f80173981b379c94
Ross Gardler <ross.gardler@microsoft.com>
2020-03-29 10:54:44 -0800

    Very basic proximity condition implemented. Add a Log behaviour (useful for testing conditions). Add an instance of the proximity check that tests if in melee range of the player.

--- Scripts/AI/Conditions/ProximityToTaggedObject.cs
+++ Scripts/AI/Conditions/ProximityToTaggedObject.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 namespace NeoFPS.AI.Condition
@@ -8,11 +9,19 @@ namespace NeoFPS.AI.Condition
     /// Test whether the agent is within minimum and maximum range of a tagged object.
     /// </summary>
     [CreateAssetMenu(fileName = "ProximityToTaggedObject", menuName = "NeoFPS/AI/Condition/Proximity To Tagged Object")]
-    public class ProximityToTaggedObject : AICondition
+    public class ProximityToTaggedObject : AbstractTaggedObjectCondition
     {
+        [SerializeField, Tooltip("Minimum distance the object must be.")]
+        float minDistance = 1.0f;
+        [SerializeField, Tooltip("Maximum distance the object must be.")]
+        float maxDistance = 1.0f;
+
         protected override bool Test()
         {
-            throw new System.NotImplementedException();
+            List<Collider> include = GetObjectsWithinSphere(maxDistance);
+            List<Collider> exclude = GetObjectsWithinSphere(minDistance);
+            IEnumerable<Collider> result = exclude.Intersect(include);
+            return result.Count<Collider>() > 0;
         }
     }
 }
