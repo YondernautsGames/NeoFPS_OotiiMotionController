5357baf9b8e434bc5961e8deed3a947f8e48f7a9
Ross Gardler <ross.gardler@microsoft.com>
2020-03-31 23:54:40 -0800

    Add field of view and line of sight to the CanSeeObject condition.

--- Scripts/AI/Conditions/CanSenseObject.cs
+++ Scripts/AI/Conditions/CanSenseObject.cs
@@ -5,28 +5,57 @@ namespace NeoFPS.AI.Condition
 {
     /// <summary>
     /// Checks to see if a particular Game Object can be sensed by the agent.
+    /// 
+    /// The agent supports a field of view. For a good tutorial on how this works see 
+    /// Sebastion Lague's Video https://www.youtube.com/watch?v=rQG9aUWarwE which was
+    /// the initial inspiration for some of the code in this script.
+    /// 
+    /// 
     /// </summary>
     [CreateAssetMenu(fileName = "CanSenseObject", menuName = "NeoFPS/AI/Condition/Can Sense Object")]
     public class CanSenseObject : AICondition
     {
-        [SerializeField, Tooltip("The name of the variable containing the game object we are attempting to sense.")]
+        [Header("Target Criteria")]
+        [SerializeField, Tooltip("The name of the variable containing a specific game object we are attempting to sense. If this is null then all objects matching the criteria below will be searched for. If this is set the search will be limited to this object.")]
         string m_VariableName = "Target";
-        [SerializeField, Tooltip("Any player within this distance of the agent will be detected regardless of their position relative to the agent.")]
-        float m_AutomaticSensingRange = 10;
+        [SerializeField, Tooltip("The layers that contain all objects that we want to sense.")]
+        LayerMask m_TargetMask;
+
+        [Header("Sight")]
+        [SerializeField, Tooltip("Any player within this radius of the agent may be seen by the agent. Note, being within this radius does not mean the object will be seen, it must also be visible via the line of sight checks. Setting this to 0 essentially makes the agent blind.")]
+        float m_ViewRadius = 10;
+        [SerializeField, Tooltip("When checking for line of sight to an object the target must be within this angle of forward on of the agent.")]
+        [Range(0, 360)]
+        float m_ViewAngle = 45;
+        [SerializeField, Tooltip("The layers that contain all objects that can obstruct line of sight to an object.")]
+        LayerMask m_LineOfSightObstacleMask;
+
+        List<Transform> sensedTargets = new List<Transform>();
+
+        private Vector3 DirectionFromAngle(float angleInDegrees)
+        {
+            return new Vector3(Mathf.Sin(angleInDegrees * Mathf.Deg2Rad), 0, Mathf.Cos(angleInDegrees * Mathf.Deg2Rad));
+        }
 
         protected override bool Test()
         {
+            sensedTargets.Clear();
+
             GameObject target = GetVariable(m_VariableName);
-            if (target == null)
-            {
-                return false;
-            }
             ObjectFilter filter = new ObjectFilter();
             filter.gameObject = target;
 
-            List<Collider> result = Senses.GetObjectsWithinSphere(m_Behaviour.m_Owner.transform.position, m_AutomaticSensingRange, filter);
+            List<Transform> result = Senses.FindVisibleTargets(m_Behaviour.m_Owner.transform, m_Behaviour.m_Owner.transform.position, m_ViewRadius, m_ViewAngle, m_LineOfSightObstacleMask, filter);
 
             return result.Count > 0;
+        }
+
+        private void OnValidate()
+        {
+            if (m_ViewRadius < 0)
+            {
+                m_ViewRadius = 0;
+            }
         }
     }
 }
--- Scripts/AI/Conditions/CanSenseTaggedObject.cs
+++ Scripts/AI/Conditions/CanSenseTaggedObject.cs
@@ -1,10 +1,12 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 namespace NeoFPS.AI.Condition
 {
     [CreateAssetMenu(fileName = "CanSenseTaggedObject", menuName = "NeoFPS/AI/Condition/Can Sense Tagged Object")]
+    [Obsolete("Use CanSeeObject instead (which at the time of writing needs support for tags adding.")]
     public class CanSenseTaggedObject : AbstractTaggedObjectCondition
     {
         [SerializeField, Tooltip("Any player within this distance of the agent will be detected regardless of their position relative to the agent.")]
